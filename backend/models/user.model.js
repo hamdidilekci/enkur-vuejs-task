eefee838a8b4e2229b6de8c65981b94deea9ba0d
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2024-09-12 17:36:07 +0300

    create user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,40 @@
+import { model, Schema } from "mongoose";
+import isemail from "isemail";
+
+// User Schema
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (v) => isemail.validate(v),
+        message: (props) => `${props.value} is not a valid email address`,
+      },
+    },
+    password: {
+      type: String,
+      default: null,
+    }
+  },
+  {
+    autoCreate: true,
+    autoIndex: true,
+    timestamps: true,
+    versionKey: false,
+    collection: "user",
+  }
+);
+
+const Model = model("User", UserSchema);
+
+export default Model;
