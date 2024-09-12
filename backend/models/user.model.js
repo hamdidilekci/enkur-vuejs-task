616dc79336ec46ed5eb95e5a3f7b0cdb60336c00
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2024-09-12 23:52:00 +0300

    add verification schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,40 +1,68 @@
 import { model, Schema } from "mongoose";
 import isemail from "isemail";
 
-// User Schema
+const verificationSchema = new Schema({
+    code: {
+        type: String,
+        default: null,
+    },
+    expireDate: {
+        type: Date,
+        default: null,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+});
+
 const UserSchema = new Schema(
-  {
-    firstName: {
-      type: String,
-      required: true,
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: (v) => isemail.validate(v),
+                message: (props) =>
+                    `${props.value} is not a valid email address`,
+            },
+        },
+        phone: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            default: null,
+        },
+        phoneVerification: { type: verificationSchema },
+        emailVerification: { type: verificationSchema },
     },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      validate: {
-        validator: (v) => isemail.validate(v),
-        message: (props) => `${props.value} is not a valid email address`,
-      },
-    },
-    password: {
-      type: String,
-      default: null,
+    {
+        autoCreate: true,
+        autoIndex: true,
+        timestamps: true,
+        collection: "user",
     }
-  },
-  {
-    autoCreate: true,
-    autoIndex: true,
-    timestamps: true,
-    versionKey: false,
-    collection: "user",
-  }
 );
 
-const Model = model("User", UserSchema);
+UserSchema.set("toJSON", {
+    transform: function (doc, ret, opt) {
+        delete ret["password"];
+        return ret;
+    },
+});
 
-export default Model;
+const User = model("User", UserSchema);
+
+export default User;
