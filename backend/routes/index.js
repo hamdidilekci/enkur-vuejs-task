f07e4ff88646c6828861de107a5ef7190eeeed96
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2024-09-13 23:24:19 +0300

    remove public and private routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,11 @@
-import express from "express";
+import { Router } from "express";
 
-import {
-  publicRouter as authPublicRoutes,
-  privateRouter as authPrivateRoutes,
-} from "./auth.routes.js";
+import authRoutes from "./auth.routes.js";
+import userRoutes from "./user.routes.js";
 
-import isAuthenticated from "../middleware/is-authenticated.js";
+const routes = Router();
 
-const router = express.Router();
+routes.use("/auth", authRoutes);
+routes.use("/user", userRoutes);
 
-router.use("/auth", authPublicRoutes);
-// To use routes below isAuthenticated you need to be an authenticated user
-router.use(isAuthenticated);
-router.use("/auth", authPrivateRoutes);
-
-export default router;
+export default routes;
