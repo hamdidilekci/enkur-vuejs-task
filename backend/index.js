89d8430b52e9c081d8ca4d7b03bde7e371ab1da8
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2024-09-13 23:17:19 +0300

    remove prop from authenticate middleware

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ import connectDatabase from "./common/connectDatabase.js";
 // allow empty strings
 mongoose.Schema.Types.String.checkRequired((v) => typeof v === "string");
 
-await connectDatabase()
+await connectDatabase();
 
 const PORT = process.env.PORT || 3001;
 
@@ -21,9 +21,9 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(authenticate({ secret: process.env.JWT_SECRET }));
+app.use(authenticate);
 app.use("/", routes);
 
 app.listen(PORT, () => {
-  console.log(`app listening on http://localhost:${PORT}`);
+    console.log(`app listening on http://localhost:${PORT}`);
 });
