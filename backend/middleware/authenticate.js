7d7e2bbd359006b49700f57ccb5d9646e43be8cf
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2024-09-13 23:20:18 +0300

    get access secret key from config, catch error cases

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -1,32 +1,41 @@
-import jwt from 'jsonwebtoken';
-import User from '../models/user.model.js';
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import sendResponse from "../common/sendResponse.js";
+import { ACCESS_SECRET_KEY } from "../config/default.js";
 
-const authenticate = ({ secret }) => {
-    return async(req, res, next) => {
-        try {
-            console.log('AUTHENTICATE.JS');
-            let token = '';
+const authenticate = async (req, res, next) => {
+    try {
+        console.log("Authenticate.js");
 
-            if (req.headers.authorization) {
-                token = req.headers.authorization.split(' ')[1];
+        const authHeader = req.headers["authorization"];
+        const token = authHeader ? authHeader.replace("Bearer ", "") : null;
+
+        if (token) {
+            const decoded = jwt.verify(token, ACCESS_SECRET_KEY);
+
+            if (!decoded || !decoded._id) {
+                throw new Error("Invalid token");
             }
 
-            if (token) {
-                const { _id } = jwt.verify(token, secret);
+            const user = await User.findOne({
+                _id: decoded._id,
+            });
 
-                const user = await User.findById(_id).lean()
-
-                if (user) {
-                    req.user = user;
-                }
+            if (!user) {
+                throw new Error("Authentication failed. User not found.");
             }
+
+            req.user = user;
         }
-        catch (error) {
-            console.error(error);
+    } catch (error) {
+        if (error.name === "TokenExpiredError") {
+            sendResponse(res, 401, {}, error.message);
+            return;
         }
 
-        next();
-    };
+        console.log("Authenticate.js error: ", error);
+    }
+    next();
 };
 
 export default authenticate;
